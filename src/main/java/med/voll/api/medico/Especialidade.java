4d9056f76cfd8455993549d2f50457241594567f
Vinicius Cretella Siqueira <vinicius.siqueira@cgu.gov.br>
2025-08-06 10:52:30 -0300

    1o commit

---
+++ src/main/java/med/voll/api/medico/Especialidade.java
@@ -0,0 +1,16 @@
+package med.voll.api.medico;
+
+public enum Especialidade {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    DERMATOLOGIA,
+    GINECOLOGIA,
+//    PEDIATRIA,
+//    PSIQUIATRIA,
+//    CLINICA_GERAL,
+//    OFTALMOLOGIA,
+//    OTORRINOLARINGOLOGIA,
+//    UROLOGIA,
+//    NEUROLOGIA,
+//    ENDROCRINOLOGIA
+}
---
+++ src/main/java/med/voll/api/paciente/PacienteController.java
@@ -0,0 +1,16 @@
+package med.voll.api.paciente;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
+        System.out.println("dados recebidos" + dados);
+    }
+}
